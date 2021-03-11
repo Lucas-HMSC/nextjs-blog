3acc562dace30813672f6b59750910f7e1abd7b3
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-03-11 16:29:56 -0300

    Dynamic Routes

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import Layout, {siteTitle} from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import {getSortedPostsData} from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -19,10 +21,10 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <strong>Lucas</strong>. I'm a JavaScript Developer and an Undergraduate Computer Science. You can contact me on <a href="https://t.me/lucas_hmsc">Telegram.</a></p>
+        <p>Hello, I'm <strong>Lucas</strong>. I'm a JavaScript Developer and an Undergraduate Computer Science. You can contact me on <a href="https://t.me/lucas_hmsc" target="_blank">Telegram.</a></p>
         <p>
           (This is a sample website - you'll be building a site like this on {' '}
-          <a href="https://nextjs.org/learn">this Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn" target="_blank">this Next.js tutorial</a>.)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
@@ -30,11 +32,13 @@ export default function Home({allPostsData}) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({id, date, title}) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
